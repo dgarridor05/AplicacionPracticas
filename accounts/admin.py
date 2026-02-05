ad6a09acd8b78a500db95b42d7a3527e636afa89
jaimegayo <jaimegayoneila@gmail.com>
2026-02-05 09:09:39 +0100

    Admin: registrar modelos y activar herramientas de gestion de grupos y perfiles

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'share_with_class')
+    list_filter = ('role', 'share_with_class')
+    search_fields = ('user__username', 'nickname')
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ClassGroup
 
-# Register your models here.
+@admin.register(ClassGroup)
+class ClassGroupAdmin(admin.ModelAdmin):
+    # Esto muestra columnas claras en la lista
+    list_display = ('name', 'teacher', 'invite_code')
+    # Esto crea un buscador por nombre de grupo o profesor
+    search_fields = ('name', 'teacher__username')
+    # Esto crea la herramienta de "dos columnas" para mover alumnos fácilmente
+    filter_horizontal = ('students',)
